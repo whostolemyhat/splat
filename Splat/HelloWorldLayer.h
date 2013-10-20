5b69037f32d22dd8cb1931433023fdf876869148
whostolemyhat <jamestease@gmail.com>
2013-10-20 10:58:39 +0100

    Initial Commit

---
+++ Splat/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Splat
+//
+//  Created by James Tease on 20/10/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Splat/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Splat
+//
+//  Created by James Tease on 20/10/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
